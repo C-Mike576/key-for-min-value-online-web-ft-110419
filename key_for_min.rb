55c108ee3bf1a2f0df47b5381a43a3716b182553
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 15:00:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,13 +6,11 @@ def key_for_min_value(name_hash)
   value = 0
   name_hash.collect do |name, number|
     value = number
-    
     if number < value
-      
       value = number
     end
-    
   end
+  binding.pry
   name_hash.each do |name, number|
     if number == value
       return name
